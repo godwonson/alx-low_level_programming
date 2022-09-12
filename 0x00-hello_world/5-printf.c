acd4cdf8b820fe3d052647b44ef383128740be49
godwonson <godwinojoko@yahoo.co.uk>
2022-09-12 05:04:04 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* Description: main - prints out a string to stdout.
+*return: 0 if success
+*/
+intvmain(void)
+{
+printf("with proper gramar, but the outcome is a piece of art, \n");
+return (0)
+}
