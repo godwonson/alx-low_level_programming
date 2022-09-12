e3e50ed5a1771395a39f7497d03612171e950798
godwonson <godwinojoko@yahoo.co.uk>
2022-09-12 04:19:46 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "programming is like building a multilingual puzzle"
-*Return: 0
+*Description: main - prints out a string to stdout.
+*Return: 0 if success.
 */
 int main(void) 
 {
